03e174272725a88dd216fe2905dac1bf8050a704
Martina Garcia <82737621+MartinaGarcia16@users.noreply.github.com>
2023-04-21 16:03:25 -0300

    Ejercicio 4 finalizado

--- practico_01/ejercicio_04.py
+++ practico_01/ejercicio_04.py
@@ -4,11 +4,22 @@
 def es_vocal_if(letra: str) -> bool:
     """Toma un string y devuelve un booleano en base a si letra es una vocal o
     no.
-
     Restricción: Utilizar un if para cada posibilidad con la función lower().
     Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
     """
-    pass # Completar
+    letra = letra.lower()
+    
+    if letra =="a":
+      return True
+    if letra == "e":
+      return True
+    if letra == "i":
+      return True
+    if letra == "o":
+      return True
+    if letra == "u":
+      return True
+    return False
 
 
 # NO MODIFICAR - INICIO
@@ -25,7 +36,9 @@ def es_vocal_if_in(letra: str) -> bool:
     """Re-escribir utilizando un sólo IF y el operador IN.
     Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations
     """
-    pass # Completar
+    if letra.lower() in "aeiou":
+      return True
+    return False
 
 
 # NO MODIFICAR - INICIO
@@ -40,7 +53,7 @@ assert es_vocal_if_in("A")
 
 def es_vocal_in(letra: str) -> bool:
     """Re-escribir utilizando el operador IN pero sin utilizar IF."""
-    pass # Completar
+    return letra.lower() in "aeiou"
 
 
 # NO MODIFICAR - INICIO
