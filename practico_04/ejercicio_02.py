ffdea1652c14e9affd302b1f33c0c8aa1c695d0f
Guillermo Dolan <75843910+guillermodolan@users.noreply.github.com>
2023-05-05 22:56:08 -0300

    Ejercicio 2 finalizado.

--- practico_04/ejercicio_02.py
+++ practico_04/ejercicio_02.py
@@ -7,7 +7,14 @@ from practico_04.ejercicio_01 import reset_tabla
 def agregar_persona(nombre, nacimiento, dni, altura):
     """Implementar la funcion agregar_persona, que inserte un registro en la 
     tabla Persona y devuelva los datos ingresados el id del nuevo registro."""
-    pass # Completar
+    conexion = sqlite3.connect("practico_04_database.db")
+    cursor = conexion.cursor()
+    contador = cursor.lastrowid
+    cursor.execute("INSERT INTO persona VALUES(?,?,?,?,?)",(contador, nombre, nacimiento, dni, altura))
+    id = cursor.lastrowid
+    conexion.commit()
+    conexion.close()
+    return id
 
 
 # NO MODIFICAR - INICIO
