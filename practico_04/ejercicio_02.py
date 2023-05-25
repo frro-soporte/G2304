d78b595e25b95da811cd0eb9044a7e1eb3a84753
Guillermo Dolan <75843910+guillermodolan@users.noreply.github.com>
2023-05-25 00:32:42 -0300

    Modificación para la fecha de nacimiento

--- practico_04/ejercicio_02.py
+++ practico_04/ejercicio_02.py
@@ -20,8 +20,8 @@ def agregar_persona(nombre, nacimiento, dni, altura):
 # NO MODIFICAR - INICIO
 @reset_tabla
 def pruebas():
-    id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
-    id_marcela = agregar_persona('marcela gonzalez', datetime.datetime(1980, 1, 25), 12164492, 195)
+    id_juan = agregar_persona('juan perez', "15/05/1988", 32165498, 180)
+    id_marcela = agregar_persona('marcela gonzalez', "25/01/1980", 12164492, 195)
     assert id_juan > 0
     assert id_marcela > id_juan
 
