0f5d6919599157e35d1ecc4bdde158acaba32464
Guillermo Dolan <75843910+guillermodolan@users.noreply.github.com>
2023-05-30 16:26:18 -0300

    Corrección de errores

--- practico_04/ejercicio_05.py
+++ practico_04/ejercicio_05.py
@@ -11,6 +11,7 @@ def actualizar_persona(id_persona, nombre, nacimiento, dni, altura):
     """Implementar la funcion actualizar_persona, que actualiza un registro de
     una persona basado en su id. Devuelve un booleano en base a si encontro el
     registro y lo actualizo o no."""
+
     conexion = sqlite3.connect("practico_04_database.db")
     cursor = conexion.cursor()
     cursor.execute("UPDATE persona SET nombre = ?, fechaNacimiento = ?, dni = ?, altura = ? WHERE idPersona = ?", (nombre, nacimiento, dni, altura, id_persona))
@@ -22,13 +23,14 @@ def actualizar_persona(id_persona, nombre, nacimiento, dni, altura):
     
     return False
 
+
 # NO MODIFICAR - INICIO
 @reset_tabla
 def pruebas():
-    id_juan = agregar_persona('juan perez', "15/05/1988", 32165498, 180)
-    actualizar_persona(id_juan, 'juan carlos perez', "16/04/1988", 32165497, 181)
-    assert buscar_persona(id_juan) == (1, 'juan carlos perez', "16/04/1988", 32165497, 181)
-    assert actualizar_persona(123, 'nadie', "16/04/1988", 12312312, 181) is False
+    id_juan = agregar_persona('juan perez', '1988-05-15', 32165498, 180)
+    actualizar_persona(id_juan, 'juan carlos perez', '1988-04-16', 32165497, 181)
+    assert buscar_persona(id_juan) == (1, 'juan carlos perez', '1988-04-16', 32165497, 181)
+    assert actualizar_persona(123, 'nadie', '1988-04-16', 12312312, 181) is False
 
 if __name__ == '__main__':
     pruebas()
